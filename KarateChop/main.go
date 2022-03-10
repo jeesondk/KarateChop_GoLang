bf473699b5ba1301e34bf0c031d1288ee1d988c7
Jesper Jensen <jesperjensen@deltek.com>
2022-03-10 10:49:14 +0100

    feat: added initial scaffold

--- KarateChop/main.go
+++ KarateChop/main.go
@@ -2,10 +2,45 @@ package KarateChop
 
 import "fmt"
 
+var _pos int = 0
+var _break bool = false
+var _source []int
+var _chop int
+
 // ChopChop BinarySearch implementation
 func ChopChop(input []int, chop int) int {
 
 	fmt.Println("Got Array: %v, chopping for: %v", input, chop)
+	if isValid(input) {
+		return -1
+	}
+
+	_source = input
+	_chop = chop
 
 	return -1
+}
+
+func chop(input []int) bool {
+	return false
+}
+
+func chopLow() []int {
+	return []int{}
+}
+
+func chopHigh() []int {
+	return []int{}
+}
+
+func resultFound() bool {
+	return false
+}
+
+func isValid(input []int) bool {
+	if input != nil {
+		return false
+	} else {
+		return true
+	}
 }
