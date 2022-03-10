83fd162a2e8a8bf31e0433102702baa81bd84eb2
Jesper Jensen <jesperjensen@deltek.com>
2022-03-10 11:45:51 +0100

    feat: added implementation 5 tests failing

--- KarateChop/main.go
+++ KarateChop/main.go
@@ -11,34 +11,66 @@ var _chop int
 func ChopChop(input []int, chop int) int {
 
 	fmt.Println("Got Array: %v, chopping for: %v", input, chop)
-	if isValid(input) {
+	if !isValid(input) {
 		return -1
 	}
 
 	_source = input
 	_chop = chop
+	_pos = chopPos(len(_source))
 
-	return -1
+	hasMatch := doChop(_source)
+	if hasMatch {
+		return _pos
+	} else {
+		return -1
+	}
 }
 
-func chop(input []int) bool {
-	return false
+func doChop(input []int) bool {
+
+	chop := chopPos(len(input))
+	valAtChop := input[chop]
+
+	if valAtChop == _chop {
+		_break = true
+	}
+
+	if len(input) <= 1 {
+		_break = true
+	}
+
+	for !_break {
+		var intSlice = make([]int, len(input))
+		if valAtChop <= _chop {
+			intSlice = chopHigh(input)
+		} else {
+			intSlice = chopLow(input)
+		}
+		doChop(intSlice)
+	}
+
+	return checkValue()
 }
 
-func chopLow() []int {
+func chopPos(arrLen int) int {
+	return arrLen / 2
+}
+
+func chopLow([]int) []int {
 	return []int{}
 }
 
-func chopHigh() []int {
+func chopHigh([]int) []int {
 	return []int{}
 }
 
-func resultFound() bool {
-	return false
+func checkValue() bool {
+	return _source[_pos] == _chop
 }
 
 func isValid(input []int) bool {
-	if input != nil {
+	if len(input) == 0 {
 		return false
 	} else {
 		return true
